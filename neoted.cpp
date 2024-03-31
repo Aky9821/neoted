5b3c12f143dfa91b45b447ad0a24bbf55a72160a
akshay nagar <akshay.nagar0101@outlook.com>
2024-03-31 21:05:11 +0530

    Added Raw Input terminal Attributes

--- neoted.cpp
+++ neoted.cpp
@@ -1,5 +1,5 @@
-#include <unistd.h>
-#include <termios.h>
+#include <unistd.h> //
+#include <termios.h> // terminal io interface
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
@@ -17,13 +17,23 @@ void enableInputMode(){
   termAttrOrigin = termAttr;
   atexit(disableRawMode);
   termAttr.c_lflag &= ~(ECHO); // sets the terminal echo bit to zero
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &termAttr); //sets modified terminal  attributes
+  termAttr.c_lflag &= ~(ICANON); // sets the terminal Canonical mode off-> reads bytes by bytes
+  termAttr.c_lflag &= ~(ISIG); // disable interupt signals 
+  termAttr.c_lflag &= ~(IXON); //disables io control ctrl s
+  termAttr.c_lflag &= ~(IEXTEN); //disables io control ctrl v
+  termAttr.c_lflag &= ~(OPOST); // disables carrage-return
+  termAttr.c_lflag &= (BRKINT); // disables SIGINT break Ctrl-c
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &termAttr); //sets modified terminal  attributes TCSAFLUSH :Change attributes when output has drained; also flush pending input.
   
 } 
 
 int main() {
   enableInputMode();
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1); // STDIN_FILENO : Standard input value, stdin. Its value is 0.
+  while (read(STDIN_FILENO, &c, 1) == 1){
+    // printf("%d ('%c')\n", c, c);
+  } //loop to read one byte at a time. STDIN_FILENO : Standard input value, stdin. Its value is 0.
+  
   return 0;
 }
